08bb5a5150a2a7f1a129cf3ccb9d9f3e56b0009c
Bogdan Peleh <peleh.b97@gmail.com>
2018-06-06 16:28:34 +0300

    add read file

---
+++ CountInvertionTests/FileServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Counting_Inversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counting_Inversion.Tests
+{
+    [TestClass()]
+    public class FileServiceTests
+    {
+        [TestMethod()]
+        public void ReadFileTest()
+        {
+            //arrange
+            int array2 = 14108;
+            var list = FileService.ReadFile();
+            //act
+            //assert
+            Assert.AreEqual(array2, list[1]);
+        }
+    }
+}
---
+++ CountInvertionTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CountInvertionTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CountInvertionTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("acd033ef-1087-454a-9e09-eaf6090be116")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
